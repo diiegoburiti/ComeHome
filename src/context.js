8b65355a87336d63f334f9d958f2f5ae40108d45
juanpereira2 <juanpereira74@hotmail.com>
2020-05-15 10:32:16 -0300

    single room started

--- src/context.js
+++ src/context.js
@@ -33,10 +33,16 @@ class RoomProvider extends Component {
     });
     return tempItems;
   }
+  //mostrar especifica room na page singleroom
+  getRoom = (slug) => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find((room) => room.slug === slug);
+    return room;
+  };
 
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
