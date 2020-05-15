46853d0494296badacdf21ea9f6124def44c6f59
juanpereira2 <juanpereira74@hotmail.com>
2020-05-15 09:53:05 -0300

    front end featured rooms

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,5 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import defaultImg from "../images/room-1.jpeg";
+import PropTypes from "prop-types";
 
-export default function Room() {
-  return <div>helo from room</div>;
+export default function Room({ room }) {
+  const { name, slug, images, price } = room;
+
+  return (
+    <article className="room">
+      <div className="img-container">
+        <img src={images[0] || defaultImg} />
+        <div className="price-top">
+          <h6>${price}</h6>
+          <p>per night</p>
+        </div>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          {" "}
+          Features
+        </Link>
+      </div>
+      <p className="room-info">{name}</p>
+    </article>
+  );
 }
+
+Room.propType = {
+  room: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    price: PropTypes.number.isRequired,
+  }),
+};
