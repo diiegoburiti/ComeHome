1eb9cce52d9f081fc30354f57212f49756005978
juanpereira2 <juanpereira74@hotmail.com>
2020-05-17 11:56:49 -0300

    room list added

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Room from "./Room";
+export default function RoomsList({ rooms }) {
+  if (rooms.lenght === 0) {
+    return (
+      <div className="empty-search">
+        <h3>unfortunately no rooms matched your search paramaters</h3>
+      </div>
+    );
+  }
 
-export default function RoomsList() {
-  return <div>hello from room list</div>;
+  return (
+    <section className="roomslist">
+      <div className="roomslist-center">
+        {rooms.map((item) => {
+          return <Room key={item.id} room={item} />;
+        })}
+      </div>
+    </section>
+  );
 }
