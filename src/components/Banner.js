1ec6b01eae4abcc3bfd17d44f93eaa5025ddd950
Diego Buriti <bs.diego97@gmail.com>
2020-10-01 21:52:53 -0300

    Div vazia do componente banner subtituido por pseudo-elemento css.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,6 @@ export default function Banner({ children, title, subtitle }) {
   return (
     <div className="banner">
       <h1>{title}</h1>
-      <div></div>
       <p>{subtitle}</p>
       {children}
     </div>
